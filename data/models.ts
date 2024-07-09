02d7b90c912662fc1585a129ee972c432d61afd6
Juan Cruz Martinez <bajcmartinez@gmail.com>
2024-07-09 23:15:52 +0200

    Add an empty state to the my courses page

--- data/models.ts
+++ data/models.ts
@@ -2,31 +2,35 @@ export type MarkdownAtom = {
   semantic_type: "markdown";
   content: {
     text: string;
-  }
-}
+  };
+};
 
 export type YouTubeAtom = {
   semantic_type: "youtube";
   content: {
     video_id: string;
-  }
-}
+  };
+};
 
-export type Atom = {
-  semantic_type: string;
-} | MarkdownAtom | YouTubeAtom;
+export type Atom =
+  | {
+      semantic_type: string;
+    }
+  | MarkdownAtom
+  | YouTubeAtom;
 
 export type Lesson = {
   slug: string;
   title: string;
   description: string;
   atoms: Atom[];
-}
+};
 
 export type Course = {
+  id: string;
   slug: string;
   title: string;
   description: string;
   level: string;
   lessons: Lesson[];
-}
+};
