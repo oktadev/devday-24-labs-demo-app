2a9dc7142e2147be8081f013f10422ad01a2aa4f
Peter Fernandez <peter.fernandez@okta.com>
2024-08-16 13:07:23 +0100

    Add invite handling

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,5 +1,35 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+import { AppRouteHandlerFnContext, handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+import { NextRequest } from 'next/server';
 
 export const GET = handleAuth({
-  signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } })
+  signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } }),
+  login: handleLogin((request) => {
+    if (request.url) {
+      const url = new URL(request.url);
+      const searchParams = url.searchParams /*new URLSearchParams(url.search);*/
+      const organization = searchParams.get("organization");
+      const invitation = searchParams.get("invitation");
+
+      if (organization && invitation) {
+        return {
+          authorizationParams: {
+            organization,
+            invitation 
+          }
+        }
+      }
+    }
+    return({});   
+  })
 });
+
+/*
+export default handleAuth({
+  signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } }),
+  invite: handleLogin({
+    authorizationParams: {
+      invitation: req.query.invitation
+    }
+  })
+});
+*/
