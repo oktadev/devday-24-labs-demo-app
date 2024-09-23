110c3420bf4970afb87f093160150f095fa41006
Sambego <sambellen@gmail.com>
2024-09-23 08:47:27 +0200

    Remove the push to the newly enrolled page to avoid race conditions with FGA

--- app/catalog/EnrollButton.tsx
+++ app/catalog/EnrollButton.tsx
@@ -1,36 +1,38 @@
-'use client';
+"use client";
 
-import { useToast } from '@/components/ui/use-toast'
-import { Button } from '@/components/ui/button'
-import { Course } from '@/data/models'
-import { useRouter } from 'next/navigation'
-import { enrollToCourse } from '@/app/actions'
+import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { Course } from "@/data/models";
+import { useRouter } from "next/navigation";
+import { enrollToCourse } from "@/app/actions";
 
 type Props = {
   course: Course;
-}
+};
 
 export default function EnrollButton({ course }: Props) {
   const { toast } = useToast();
   const router = useRouter();
 
   return (
-    <Button onClick={async () => {
-      const result = await enrollToCourse(course.slug)
-      if (result.success) {
-        toast({
-          title: result.message,
-        });
-        router.push(`/my-courses/${course.slug}`);
-      } else {
-        toast({
-          title: result.message,
-          description: "This message is to ensure we are properly validating the user session on the server side and not only on the front end.",
-          variant: 'destructive',
-        });
-      }
-    }}>
+    <Button
+      onClick={async () => {
+        const result = await enrollToCourse(course.slug);
+        if (result.success) {
+          toast({
+            title: result.message,
+          });
+        } else {
+          toast({
+            title: result.message,
+            description:
+              "This message is to ensure we are properly validating the user session on the server side and not only on the front end.",
+            variant: "destructive",
+          });
+        }
+      }}
+    >
       Enroll
     </Button>
-  )
+  );
 }
